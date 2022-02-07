be1870dc06b5a53884991ea1c7002e63ef7c97be
hoangbao321 <nglehoangbao@gmail.com>
2022-02-07 18:02:54 +0700

    null || nullable

---
+++ cs18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace cs18
+{
+    class Program
+    {
+        class Abc
+        {
+            public void Xinchao() => Console.WriteLine("xin chào c#");
+            public void Abcinfo() => Console.WriteLine("info C#"); 
+        }
+        static void Main(string[] args)
+        {
+            // null sử dụng cho kiểu tham chiếu
+            int? age;
+            age = null;
+            int a = 10;
+            if (age != null)
+            {
+                int _age = (int)age;
+            }
+        }
+    }
+}
