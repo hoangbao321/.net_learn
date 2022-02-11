789343abd2268b97294faae3dccef86cdb8e4adb
hoangbao321 <nglehoangbao@gmail.com>
2022-02-11 16:54:00 +0700

    extension method cs24

---
+++ cs_extensionmethod/MyLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public static class XYZ
+    {
+        public static double Binhphuong(this double x) => x * x;
+        public static double Canbachai(this double x) => Math.Sqrt(x);
+        public static double Sin(this double x) => Math.Sin(x);
+    }
+}
---
+++ cs_extensionmethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using MyLib;
+namespace cs_extensionmethod
+{
+    //extension method
+    // lớp tĩnh
+    static class ABC
+    {
+        public static void Print(this string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "Xin".Print(ConsoleColor.DarkGreen);
+            "Chao".Print(ConsoleColor.DarkMagenta);
+            "Cac".Print(ConsoleColor.Cyan);
+            "Ban".Print(ConsoleColor.DarkYellow);
+
+            double a = 2.5;
+            Console.WriteLine(a.Binhphuong());
+            Console.WriteLine(a.Sin());
+            Console.WriteLine(a.Canbachai());
+        }
+    }
+}
