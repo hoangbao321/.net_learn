11b062331ab7c9971ab702d26b24d2a4aaac5f67
hoangbao321 <nglehoangbao@gmail.com>
2022-02-11 16:58:53 +0700

    cs23_cs_extensionmethod

--- cs_extensionmethod/MyLib.cs
+++ cs_extensionmethod/MyLib.cs
@@ -11,5 +11,6 @@ namespace MyLib
         public static double Binhphuong(this double x) => x * x;
         public static double Canbachai(this double x) => Math.Sqrt(x);
         public static double Sin(this double x) => Math.Sin(x);
+        public static double Tan(this double x) => Math.Tan(x);
     }
 }
