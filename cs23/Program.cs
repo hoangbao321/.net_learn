ad62957025844f21ef13dfc9b4232a7267378a08
hoangbao321 <nglehoangbao@gmail.com>
2022-03-07 20:56:06 +0700

    Extension method

---
+++ cs23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cs23
+{
+    public static class Extension
+    {
+        public static void Print(this string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+            Console.ResetColor();
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            "hello".Print(ConsoleColor.DarkCyan);
+            Console.WriteLine(5.Tinhbinhphuong());
+            Console.WriteLine(7.Tinhbinhphuong());
+            Console.WriteLine(8.Tinhbinhphuong());
+        }
+    }
+}
---
+++ cs23/TinhToan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs23
+{
+    public static class Tinhtoan
+    {
+        public static int Tinhbinhphuong(this int number) => number * number;
+    }
+}
