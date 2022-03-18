6b26e051dbad2bba09f3f573217032070ce56e17
hoangbao321 <nglehoangbao@gmail.com>
2022-03-18 16:39:34 +0700

    test DI on .net

---
+++ cs33_DI_WEB/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using cs33_DI_WEB.Models;
+
+namespace cs33_DI_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ProductService _product;
+
+        public HomeController(ILogger<HomeController> logger,ProductService productservice )
+        {
+            _logger = logger;
+            _product = productservice;
+        }
+        public IActionResult ProductInfo(string ID)
+        {
+            var sp = _product.FindSP(ID);
+            return View(sp);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ cs33_DI_WEB/Service/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Product
+{
+    public string ID { get; set; }
+    public string Name { get; set; }
+}
+public class ProductService
+{
+   
+    List<Product> ds_sp = new List<Product>();
+    public ProductService()
+    {
+        Console.WriteLine("is created");
+        ds_sp.AddRange(new Product[] { 
+            new Product(){ID="1",Name ="Iphone"},
+            new Product(){ID="2",Name ="Ipad"},
+            new Product(){ID="3",Name ="Macbook"},
+        });
+    }
+    public Product FindSP(string productID)
+    {
+        var sp = from p in ds_sp
+                 where p.ID == productID
+                 select p;
+
+        return sp.FirstOrDefault();
+    }
+}
+
