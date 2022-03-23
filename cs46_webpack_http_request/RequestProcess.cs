dfee3722999e1f0c6b7b0784ef2069d5a670ffb0
hoangbao321 <nglehoangbao@gmail.com>
2022-03-23 15:29:16 +0700

    cs46

---
+++ cs46_webpack_http_request/RequestProcess.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace cs46_webpack_http_request
+{
+
+    public static class RequestProcess
+    {
+        public static string ProcessForm(HttpRequest request)
+        {
+            string hovaten = "";
+            bool luachon = false;
+            string email = "";
+            string password = "";
+            string thongbao = "";
+            if(request.Method == "POST")
+            {
+                IFormCollection _form = request.Form;
+                email = _form["email"].FirstOrDefault() ?? "email bo trong";
+                hovaten = _form["hovaten"].FirstOrDefault() ?? "ho ten bo trong";
+                password = _form["password"].FirstOrDefault() ?? "dasdsaasdasd";
+                luachon = (_form["luachon"].FirstOrDefault() == "on");
+
+                thongbao = $@"Du lieu POST - email: {email}"
+                       + $"-hovaten: {hovaten}-password:{password}" 
+                       + $"-luachon: {luachon}";
+
+                if (!Directory.Exists("wwwroot/upload/")) Directory.CreateDirectory("wwwroot/upload/");
+
+                string thongbaofile = "";
+                if ( _form.Files.Count > 0)
+                {
+                    thongbaofile = "cac file da upload: <br>";
+                    foreach (var file in _form.Files)
+                    {
+                        string file_name = $"{file.FileName}";
+                        string filepath = "wwwroot/upload/" + file.FileName;
+                        using ( var stream = new FileStream(filepath,FileMode.CreateNew))
+                        {
+                            file.CopyTo(stream);
+                        }
+                        thongbaofile += file_name+"--"+file.Length+ "bytes <br>";
+                    }
+                }
+                thongbao += thongbaofile;
+            }
+
+
+            var format = File.ReadAllText("formtest.html");
+
+            var html =string.Format(format, hovaten, email, luachon ? "checked" :"" )  + thongbao;
+            
+            return html.HtmlTag("div", "container");
+        }
+        public static string RequestInfo(HttpRequest request)
+        {
+            // Đọc các thông tin cơ bản của Request
+            // Trả về HTML trình bày các thông tin đó
+            var sb = new StringBuilder();
+
+            // Lấy http scheme (http|https)
+            var scheme = request.Scheme;
+            sb.Append( ("scheme".td() + scheme.td()).tr() );
+
+            // HOST Header
+            var host = (request.Host.HasValue ? request.Host.Value : "no host");
+            sb.Append( ("host".td() + host.td()).tr() );
+
+
+            // Lấy pathbase (URL Path - cho Map)
+            var pathbase = request.PathBase.ToString();
+            sb.Append( ("pathbase".td() + pathbase.td()).tr() );
+
+            // Lấy Path (URL Path)
+            var path = request.Path.ToString();
+            sb.Append(("path".td() + path.td()).tr());
+
+            // Lấy chuỗi query của URL
+            var QueryString = request.QueryString.HasValue ? request.QueryString.Value : "no query string";
+            sb.Append(("QueryString".td() + QueryString.td()).tr());
+
+            // Lấy phương thức
+            var method = request.Method;
+            sb.Append(("Method".td() + method.td()).tr());
+
+            // Lấy giao thức
+            var Protocol = request.Protocol;
+            sb.Append(("Protocol".td() + Protocol.td()).tr());
+
+            // Lấy ContentType
+            var ContentType = request.ContentType;
+            sb.Append(("ContentType".td() + ContentType.td()).tr());
+
+            // Lấy danh sách các Header và giá trị  của nó, dùng Linq để lấy
+            // Header gửi đến lưu trong thuộc tính Header  kiểu Dictionary
+            var listheaderString = request.Headers.Select((header) => $"{header.Key}: {header.Value}".HtmlTag("li"));
+            var headerhmtl = string.Join("", listheaderString).HtmlTag("ul"); // nối danh sách thành 1
+            sb.Append(("Header".td() + headerhmtl.td()).tr());
+
+            // Lấy danh sách các Header và giá trị  của nó, dùng Linq để lấy
+            var listcokie = request.Cookies.Select((header) => $"{header.Key}: {header.Value}".HtmlTag("li"));
+            var cockiesHtml = string.Join("", listcokie).HtmlTag("ul");
+            sb.Append(("Cookies".td() + cockiesHtml.td()).tr());
+
+
+            // Lấy tên và giá trí query
+            var listquery = request.Query.Select((header) => $"{header.Key}: {header.Value}".HtmlTag("li"));
+            var queryhtml = string.Join("", listquery).HtmlTag("ul");
+            sb.Append(("Các Query".td() + queryhtml.td()).tr());
+
+            //Kiểm tra thử query tên zzz có không
+            Microsoft.Extensions.Primitives.StringValues zzzz;
+            bool existabc = request.Query.TryGetValue("zzzz", out zzzz);
+            string queryVal = existabc ? zzzz.FirstOrDefault() : "không có giá trị";
+            sb.Append(("zzz query".td() + queryVal.ToString().td()).tr());
+
+            string info = "Thông tin Request".HtmlTag("h2") + sb.ToString().HtmlTag("table", "table table-sm table-bordered");
+            return info;
+        }
+    }
+}
---
+++ cs46_webpack_http_request/Startup.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace cs46_webpack_http_request
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    // tạo menu
+                    var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+                    var html = HtmlHelper.HtmlDocument("Xin chao", menu + HtmlHelper.HtmlTrangchu());
+                    await context.Response.WriteAsync(html);
+                });
+
+                endpoints.MapGet("/RequestInfo", async context =>
+                {
+                    var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+
+                    var info = RequestProcess.RequestInfo(context.Request).HtmlTag("div", "container");
+
+                    var html = HtmlHelper.HtmlDocument("Thong tin RequestInfo", menu + info);
+
+                    await context.Response.WriteAsync(html);
+                });
+
+                endpoints.MapGet("/Encoding", async context =>
+                {
+                    context.Response.Headers.Add("asa", "bbb");
+                    await context.Response.WriteAsync("Encoding");
+                });
+
+                //  /Cookies/ write
+                //  /Cookies/read
+                endpoints.MapGet("/Cookies/{*action}", async context =>
+                {
+                    // tạo menu
+                    var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+
+                    // tạo mấy cái nút
+                    var huongdan = @$"<a class=""btn btn-danger""  href=""/Cookies/read"">Doc Cookies</a>
+                                    <a class=""btn btn-success"" href=""/Cookies/write"">Ghi Cookies</a>";
+
+                    var action = context.GetRouteValue("action") ?? "read";
+
+                    var message = "";
+                    if (action.ToString() == "write")
+                    {
+                        var option = new CookieOptions()
+                        {
+                            Path = "/abc",
+                            Expires = DateTime.Now.AddDays(1),
+                        };
+                        // ten-gia tri
+                        context.Response.Cookies.Append("masanpham", "dasdasda",option);
+                        message = "Cookies duoc ghi";
+                    }
+                    else
+                    {
+                        var listcokie = context.Request.Cookies.Select((header) => $"{header.Key}: {header.Value}".HtmlTag("li"));
+                        message = string.Join("", listcokie).HtmlTag("ul");
+                    }
+
+                    message = message.HtmlTag("div", "alert alert-danger");
+
+                    huongdan = huongdan.HtmlTag("div", "container mt-4");
+
+                    var html = HtmlHelper.HtmlDocument("COOKIES: " + action, menu + huongdan + message);
+
+                    await context.Response.WriteAsync(html);
+                });
+
+                endpoints.MapGet("/Json", async context =>
+                {
+                    var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+
+                    var p = new
+                    {
+                        TenSp = "Dong ho Abc",
+                        Gia = 500000,
+                        NgaySX = new DateTime(2022, 3, 21)
+                    };
+
+
+                    var json = JsonConvert.SerializeObject(p).HtmlTag("div", "container");
+
+                    var html = HtmlHelper.HtmlDocument("JSON", menu + json);
+
+                    await context.Response.WriteAsync(html);
+                });
+
+                endpoints.MapMethods("/Form", new string[] { "GET", "POST" }, async context =>
+                   {
+                       var menu = HtmlHelper.MenuTop(HtmlHelper.DefaultMenuTopItems(), context.Request);
+                       var htmlform = RequestProcess.ProcessForm(context.Request);
+                       var html = HtmlHelper.HtmlDocument("Test Submit form", menu + htmlform);
+                       await context.Response.WriteAsync(html);
+                   });
+
+            });
+        }
+    }
+}
