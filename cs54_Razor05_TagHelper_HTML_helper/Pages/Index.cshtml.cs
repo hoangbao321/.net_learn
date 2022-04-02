dfc14bb72a6da87e890b35268ee75374622d6fec
hoangbao321 <nglehoangbao@gmail.com>
2022-04-02 21:59:31 +0700

    2/4/2022

---
+++ cs54_Razor05_TagHelper_HTML_helper/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace cs54_Razor05_TagHelper_HTML_helper.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [DisplayName("ten nguoi dung")]
+        public string username { get; set; } ="nglehoangbao";
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ cs54_Razor05_TagHelper_HTML_helper/Pages/ViewProduct.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cs54_Razor05_TagHelper_HTML_helper.Pages
+{
+    public class ViewProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
