a07cc75fa31547d4f200483517b3b676ad5aab42
hoangbao321 <nglehoangbao@gmail.com>
2022-04-02 21:54:56 +0700

    Layout section

---
+++ cs51_Razor_02/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace cs51_Razor_02.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Abc { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Abc = "Onget dsada";
+        }
+    }
+}
