ed8f5ab30763e4be7d6006aefd90ba722ede33e6
hoangbao321 <nglehoangbao@gmail.com>
2022-04-02 21:58:07 +0700

    component

---
+++ cs52_Razor_03/Pages/Post2.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cs52_Razor_03.Pages
+{
+    public class Post2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ cs52_Razor_03/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace cs52_Razor_03.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
